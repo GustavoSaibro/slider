e232ab11e40c30d69a1b00a6f9591b9b82b6687c
Gustavo saibro <gustavosaibro@gmail.com>
2022-04-02 19:11:34 -0300

    Doing the basic setup to start to build components

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,45 @@
 import { createStore } from 'vuex'
+// import { getSrc } from '@/utils/util.js'
 
-export default createStore({
+
+const store = createStore({
   state: {
+    content: [
+      {
+        headline: 'Teste',
+        paragraph: 'this is a paragraph',
+        img: 'img.jpg'
+      },
+      {
+        headline: 'Teste2',
+        paragraph: 'this is a paragraph',
+        img: 'img2.jpg'
+      },
+      {
+        headline: 'Teste3',
+        paragraph: 'this is a paragraph',
+        img: 'img3.jpg'
+      },
+      {
+        headline: 'Teste4',
+        paragraph: 'this is a paragraph',
+        img: 'img4.jpg'
+      },
+      {
+        headline: 'Teste5',
+        paragraph: 'this is a paragraph',
+        img: 'img5.jpg'
+      },
+
+    ]
   },
-  mutations: {
+  getters: {
+    getContent (state) {
+      return state.content
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  mutations: {},
+  actions: {}
 })
+
+export default store
